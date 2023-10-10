4d2f1b56ad6b9d524095cc9285d57c2613777ad5
Ethan DeSon <ethandeson@gmail.com>
2023-10-10 15:16:45 -0700

    basic graph

--- main.cs
+++ main.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Xml;
+using Microsoft.VisualBasic;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        Graph graph = new();
+        Graph.Node A = new();
+        Graph.Node B = new();
+        Graph.Edge AtoB = new()
+        {
+            costs = new System.Collections.Generic.List<int> { 1, 1 },
+            source = A,
+            target = B
+        };
+        A.edges.Add(AtoB);
+
+        Graph.Edge BtoA = new()
+        {
+            costs = new System.Collections.Generic.List<int> { 1, 1 },
+            source = B,
+            target = A
+        };
+        B.edges.Add(BtoA);
+
+        graph.AddNode(A);
+        graph.AddNode(B);
+
     }
 }
